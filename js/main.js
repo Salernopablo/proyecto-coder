b98c8c99d31621dcd2251598004d17be07023265
Pablo Salerno <pablosalerno.1996@gmail.com>
2024-09-17 21:06:10 -0300

    Declaracion de funciones y uso de while, y switch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-prompt("Hola! como te llamas?")
+let nombre = prompt("Escribe tu nombre");
+
+function saludar(){
+    alert ("Bienvenido "+nombre+"!!!")
+    console.log ("Bienvenido "+nombre+" a el centro de atencion médica personalizada")
+}
+saludar();
+
+function sacarTurno(){
+    let fecha = prompt ("Elige la fecha en la que te gustaria tener un turno");
+    alert ("Elegiste la fecha: "+fecha);
+}
+
+function tengoTurno(){
+    alert ("Función para mostrar los detalles de tu turno actual (puedes definir qué hacer aquí)");
+}
+
+let reservar = parseInt(prompt(nombre+", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
+while (reservar !== 3){
+    switch (reservar){
+        case 1:
+            sacarTurno();
+            break;
+        case 2:
+            tengoTurno();
+            break;
+        default:
+            alert("Opcion incorrecta");
+    }
+    reservar = parseInt(prompt(nombre + ", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
+}
+alert("Gracias por usar el sistema. ¡Hasta luego!");
