1d13dd5b0fbcd727e498793a183b9bce1a606d95
Pablo Salerno <pablosalerno.1996@gmail.com>
2024-09-18 14:17:03 -0300

    Arrays agregados

--- js/main.js
+++ js/main.js
@@ -6,13 +6,29 @@ function saludar(){
 }
 saludar();
 
-function sacarTurno(){
-    let fecha = prompt ("Elige la fecha en la que te gustaria tener un turno");
-    alert ("Elegiste la fecha: "+fecha);
+const turnosDisponibles = ["13.30 hs", "14.30 hs", "16.30 hs", "17 hs"];
+
+let usuario = {
+    nombre,
+    apellido: "", 
+    edad: 0,
 }
 
+function sacarTurno(){
+    let esRegistrado = confirm ("¿Ya res paciente registrado?");
+    if (esRegistrado){
+        let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
+        alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
+    }else{
+        usuario.apellido = prompt ("Escribe tu apellido");
+        usuario.edad = parseInt(prompt("Dinos, "+nombre+" ¿cuantos años tienes?"));
+        alert ("Gracias por registrarte "+nombre+" "+usuario.apellido+". Ahora puedes elegir un turno.");
+        let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
+        alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
+    }
+}
 function tengoTurno(){
-    alert ("Función para mostrar los detalles de tu turno actual (puedes definir qué hacer aquí)");
+    alert ("Estos son detalles de tu turno actual");
 }
 
 let reservar = parseInt(prompt(nombre+", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
@@ -30,3 +46,5 @@ while (reservar !== 3){
     reservar = parseInt(prompt(nombre + ", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
 }
 alert("Gracias por usar el sistema. ¡Hasta luego!");
+
+
