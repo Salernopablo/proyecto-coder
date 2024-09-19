98e72869ea825576a239e7c000a1edc73ecb950a
Pablo Salerno <pablosalerno.1996@gmail.com>
2024-09-19 12:07:00 -0300

    Nuevos console.log, pedidos de parametros, y verificacion de turnos aplicados o no a un usuario

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-function saludar(){
+let nombre = prompt("Escribe tu nombre");
+saludar(nombre);
+
+function saludar(nombre){
     alert ("Bienvenido "+nombre+"!!!")
     console.log ("Bienvenido "+nombre+" a el centro de atencion médica personalizada")
 }
 
-let nombre = prompt("Escribe tu nombre");
-saludar(nombre);
-
-const turnosDisponibles = ["13.30 hs", "14.30 hs", "16.30 hs","17 hs"];
+const turnosDisponibles = ["13 hs", "14 hs", "16 hs","17 hs"];
 
 for (let i = 0; i < turnosDisponibles.length; i++) {
-    console.log(turnosDisponibles[i].id + " - " + turnosDisponibles[i].hora);
+    console.log(turnosDisponibles[i]);
 }
 
 let usuario = {
@@ -17,36 +17,50 @@ let usuario = {
     apellido: "", 
     dni: 0,
     mail: "",
+    turnoActual: null,
 }
 
-function sacarTurno(){
+function sacarTurno(turnosDisponibles){
     let esRegistrado = confirm ("¿Ya eres paciente registrado?");
 
     if (esRegistrado){
-        let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
+        parseInt(prompt("Ingrese su número de DNI"))
+        let turnoElegido = prompt ("Ahora elija uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
         alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
+        usuario.turnoActual = turnoElegido;
+        console.log("Turno elegido por el usuario registrado: " + turnoElegido);
     }else{
         usuario.nombre = prompt ("Escribe tu nombre")
         usuario.apellido = prompt ("Escribe tu apellido");
         usuario.dni = parseInt(prompt(usuario.nombre+" escribe tu numero de DNI"));
         usuario.mail = prompt ("Por ultimo, regitra el mail a donde quieres que te enviemos la confirmacion del turno")
-        alert ("Gracias por registrarte "+nombre+" "+usuario.apellido+". Ahora puedes elegir un turno.");
+        alert ("Gracias por registrarte "+usuario.nombre+" "+usuario.apellido+". Ahora puedes elegir un turno.");
+        console.log("Datos del usuario después del registro: ", usuario);
         let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
-        alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
+        alert ("¡Perfecto "+usuario.nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
+        usuario.turnoActual = turnoElegido;
+        console.log("Turno elegido por el usuario no registrado: " + turnoElegido);
     }
 }
-function tengoTurno(){
-    alert ("Estos son detalles de tu turno actual: ");
+function tengoTurno(usuario){
+    if(usuario.turnoActual){
+        alert ("Estos son detalles de tu turno actual: Tienes turno reservado para la hora " +usuario.turnoActual);
+        console.log("El usuario tiene un turno reservado: " + usuario.turnoActual);
+    } else {
+        alert ("No tienes turno");
+        console.log("El usuario no tiene ningún turno reservado.");
+    }
+    
 }
 
 let reservar = parseInt(prompt(nombre+", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
 while (reservar !== 3){
     switch (reservar){
         case 1:
-            sacarTurno();
+            sacarTurno(turnosDisponibles);
             break;
         case 2:
-            tengoTurno();
+            tengoTurno(usuario);
             break;
         default:
             alert("Opcion incorrecta, por favor elige una opción válida.");
