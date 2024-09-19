d9cba69bf18727bcb037d6ea56fed885b87261a9
Pablo Salerno <pablosalerno.1996@gmail.com>
2024-09-19 08:34:54 -0300

    Cambios en los requisitos del registro para usuarios

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
-let nombre = prompt("Escribe tu nombre");
-
 function saludar(){
     alert ("Bienvenido "+nombre+"!!!")
     console.log ("Bienvenido "+nombre+" a el centro de atencion médica personalizada")
 }
-saludar();
 
-const turnosDisponibles = ["13.30 hs", "14.30 hs", "16.30 hs", "17 hs"];
+let nombre = prompt("Escribe tu nombre");
+saludar(nombre);
+
+const turnosDisponibles = ["13.30 hs", "14.30 hs", "16.30 hs","17 hs"];
+
+for (let i = 0; i < turnosDisponibles.length; i++) {
+    console.log(turnosDisponibles[i].id + " - " + turnosDisponibles[i].hora);
+}
 
 let usuario = {
-    nombre,
+    nombre: "",
     apellido: "", 
-    edad: 0,
+    dni: 0,
+    mail: "",
 }
 
 function sacarTurno(){
-    let esRegistrado = confirm ("¿Ya res paciente registrado?");
+    let esRegistrado = confirm ("¿Ya eres paciente registrado?");
+
     if (esRegistrado){
         let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
         alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
     }else{
+        usuario.nombre = prompt ("Escribe tu nombre")
         usuario.apellido = prompt ("Escribe tu apellido");
-        usuario.edad = parseInt(prompt("Dinos, "+nombre+" ¿cuantos años tienes?"));
+        usuario.dni = parseInt(prompt(usuario.nombre+" escribe tu numero de DNI"));
+        usuario.mail = prompt ("Por ultimo, regitra el mail a donde quieres que te enviemos la confirmacion del turno")
         alert ("Gracias por registrarte "+nombre+" "+usuario.apellido+". Ahora puedes elegir un turno.");
         let turnoElegido = prompt ("Elige uno de los siguientes turnos disponibles: "+turnosDisponibles.join("\n"));
         alert ("¡Perfecto "+nombre+" "+usuario.apellido+"! haz elegido el turno de la hora "+turnoElegido);
     }
 }
 function tengoTurno(){
-    alert ("Estos son detalles de tu turno actual");
+    alert ("Estos son detalles de tu turno actual: ");
 }
 
 let reservar = parseInt(prompt(nombre+", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
@@ -41,10 +49,8 @@ while (reservar !== 3){
             tengoTurno();
             break;
         default:
-            alert("Opcion incorrecta");
+            alert("Opcion incorrecta, por favor elige una opción válida.");
     }
     reservar = parseInt(prompt(nombre + ", por favor, elige una opción: \n 1-Sacar un turno \n 2-Tengo un turno \n 3-Salir"));
 }
 alert("Gracias por usar el sistema. ¡Hasta luego!");
-
-
